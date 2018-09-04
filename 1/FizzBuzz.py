9ca680042cf7216036fb3e1e95577ab71c849c78
ameenkhan07 <ameenkhan07@gmail.com>
2018-09-04 02:18:24 -0400

    FizzBuzz 1.0 format yapf

--- 1/FizzBuzz.py
+++ 1/FizzBuzz.py
@@ -1,9 +1,9 @@
-# Software 1.0 Approach
+"""Software 1.0 Approach"""
 
 import unittest
 
-class FizzBuzz():
 
+class FizzBuzz():
     def func(self, n):
         """Return string representation of input number with following exceptions: 
             - if a number is a multiple of 3, return 'Fizz'
@@ -12,35 +12,34 @@ class FizzBuzz():
         :type n: int
         :rtype: str
         """
-        if not n : return None
+        if not n: return None
 
         if not n % 5 and not n % 3:
-            return('FizzBuzz')
+            return ('FizzBuzz')
         elif not n % 3:
-            return('Fizz')
-        elif not n % 5: 
-            return('Buzz')
+            return ('Fizz')
+        elif not n % 5:
+            return ('Buzz')
         else:
             return str(n)
-        
-class TestFizzBuzz(unittest.TestCase):
 
+
+class TestFizzBuzz(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(FizzBuzz().func(3), 'Fizz')
 
     def test_buzz(self):
         self.assertEqual(FizzBuzz().func(10), 'Buzz')
-    
+
     def test_fizzbuzz(self):
         self.assertEqual(FizzBuzz().func(30), 'FizzBuzz')
-    
+
     def test_none(self):
         self.assertEqual(FizzBuzz().func(None), None)
-    
+
     def test_seven(self):
-        self.assertEqual(FizzBuzz().func(7),'7')
+        self.assertEqual(FizzBuzz().func(7), '7')
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     unittest.main()
-
-        
