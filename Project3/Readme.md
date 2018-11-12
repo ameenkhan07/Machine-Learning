9670019a59ff64ee95aa92206764b6268aeef10f
ameenkhan07 <ameenkhan07@gmail.com>
2018-11-12 14:07:03 -0500

    Added softmax regression for MNIST

---
+++ Project3/Readme.md
@@ -0,0 +1,13 @@
+
+### Setup
+
+```{sh}
+# Setup venv
+python3 -m venv virtualenv
+source virtualenv/bin/activate
+
+# Install all libraries
+pip install jupyter --user
+pip3 install --upgrade tensorflow
+pip3 install pandas numpy keras scikit-learn matplotlib
+```
---
+++ Project3/process_data.py
@@ -0,0 +1,52 @@
+import pickle
+import gzip
+import numpy as np
+import os
+from PIL import Image
+
+MNIST_data_files = 'mnist.pkl.gz'
+USPS_data_files = 'USPSdata/Numerals'
+
+
+def encode(l):
+    """Encode labels from 1-10 to binary alternatives
+    """
+    return((np.arange(10) == l[:, None]).astype(np.float32))
+
+
+def get_MNIST_data():
+    """
+    """
+    f = gzip.open(MNIST_data_files, 'rb')
+    train_data, validation_data, test_data = pickle.load(
+        f, encoding='latin1')
+    f.close()
+    # Getting Datasets and Targets
+    train_tar, validation_tar, test_tar = train_data[1], validation_data[1], test_data[1]
+    train_data, validation_data, test_data = train_data[0], validation_data[0], test_data[0]
+    # Encoded Targets to labels
+    train_labels, validation_labels, test_labels = encode(
+        train_tar), encode(validation_tar), encode(test_tar)
+    return(train_data, train_tar, train_labels,
+           validation_data, validation_tar, validation_labels,
+           test_data, test_tar, test_labels)
+
+
+def get_USPS_data():
+    """
+    """
+    USPSMat, USPSTar = [], []
+    savedImg, labels = [], []
+    for j in range(0, 10):
+        curFolderPath = USPS_data_files + '/' + str(j)
+        imgs = os.listdir(curFolderPath)
+        for img in imgs:
+            curImg = curFolderPath + '/' + img
+            if curImg[-3:] == 'png':
+                img = Image.open(curImg, 'r')
+                img = img.resize((28, 28))
+                # NORMALIZE
+                imgdata = (255-np.array(img.getdata()))/255
+                USPSMat.append(imgdata)
+                USPSTar.append(j)
+    return(USPSMat, USPSTar)
